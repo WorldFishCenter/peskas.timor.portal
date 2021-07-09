9beb0b46e6f554f9442d4e01edc7b23d57164957
Fernando Cagua <efcaguab@gmail.com>
2021-07-09 16:54:36 +1200

    Make it so it doesn't depend on devtools

--- app.R
+++ app.R
@@ -2,5 +2,7 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-peskas.timor.portal::run_app() # add parameters here (if any)
+# pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
+files.sources <- list.files("R")
+sapply(files.sources, source)
+run_app() # add parameters here (if any)
