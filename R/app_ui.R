53a3d6e88004517618e7b6458540f76fd6c2c389
Fernando Cagua <efcaguab@gmail.com>
2021-07-09 15:55:24 +1200

    Add basic UI

--- R/app_ui.R
+++ R/app_ui.R
@@ -5,12 +5,29 @@
 #' @import shiny
 #' @noRd
 app_ui <- function(request) {
-  tagList(
-    # Leave this function for adding external resources
-    # Your application UI logic
-    fluidPage(
-      h1("peskas.timor.portal")
+  tabler_page(
+    title = "Peskas | East Timor",
+    header(
+      logo = peskas_logo(),
+      version_flex(
+        heading = "Managment Dashboard",
+        subheading = "East Timor (v0.0.12-alpha)"
+      )
+    ),
+    tab_menu(
+    tab_menu_item("Home", "home"),
+    tab_menu_item("About", "about")
+    ),
+    tabset_panel(
+      tab_panel(
+        id = "home",
+        page_header(pretitle = "Small scale fisheries", title = "Overview"),
+
+      ),
+      tab_panel(
+        id = "about",
+        page_header(pretitle = "", title = "About")
+      )
     )
   )
 }
-
