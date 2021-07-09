039706a92252085f81530ac657ed4a50ac05c831
Fernando Cagua <efcaguab@gmail.com>
2021-07-09 15:50:56 +1200

    Copy sys fucntion from tabler

---
+++ R/app_config.R
@@ -0,0 +1,13 @@
+#' Access files in the current app
+#'
+#' NOTE: If you manually change your package name in the DESCRIPTION,
+#' don't forget to change it here too, and in the config file.
+#' For a safer name change mechanism, use the `golem::set_golem_name()` function.
+#'
+#' @param ... character vectors, specifying subdirectory and file(s)
+#' within your package. The default, none, returns the root of the app.
+#'
+#' @noRd
+app_sys <- function(...){
+  system.file(..., package = "peskas.timor.portal")
+}
