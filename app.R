1083d464d9140ba2e5bd35d996bb7b0ee1625a11
Fernando Cagua <efcaguab@gmail.com>
2021-07-09 17:28:11 +1200

    Use normal pkgload

--- app.R
+++ app.R
@@ -2,7 +2,5 @@
 # To deploy, run: rsconnect::deployApp()
 # Or use the blue button on top of this file
 
-# pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
-files.sources <- list.files("R", full.names = T)
-sapply(files.sources, source)
-run_app() # add parameters here (if any)
+pkgload::load_all(export_all = FALSE,helpers = FALSE,attach_testthat = FALSE)
+peskas.timor.portal:::run_app() # add parameters here (if any)
