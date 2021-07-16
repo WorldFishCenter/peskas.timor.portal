16c39065105c50afa440590322a0f84bbd74c99d
Fernando Cagua <efcaguab@gmail.com>
2021-07-16 12:53:03 +1200

    Create template for a card page

---
+++ R/ui_card_page.R
@@ -0,0 +1,12 @@
+page_cards <- function(...){
+  tags$div(
+    class = "page-body",
+    tags$div(
+      class = "container-xl",
+      tags$div(
+        class = "row row-deck row-cards",
+        ...
+      )
+    )
+  )
+}
