dd6d85572ed95f1b0f10bacc40c5b79d8d5f39af
Fernando Cagua <efcaguab@gmail.com>
2021-07-26 19:42:14 +1200

    Remove downloaded file

--- data-raw/aggregated.R
+++ data-raw/aggregated.R
@@ -1,6 +1,7 @@
 ## code to prepare `aggregated` dataset goes here
 pkgload::load_all()
 
+# Download file
 pars <- config::get(file = "inst/golem-config.yml")
 aggregated_rds <- cloud_object_name(
   prefix = "timor_aggregated",
@@ -8,12 +9,12 @@ aggregated_rds <- cloud_object_name(
   extension = "rds",
   options = pars$storage$google$options,
   exact_match = TRUE)
-
 download_cloud_file(name = aggregated_rds,
                     provider = pars$storage$google$key,
                     options = pars$storage$google$options)
-
 aggregated <- readRDS(aggregated_rds)
+file.remove(aggregated_rds)
+
 aggregated <- lapply(aggregated, data.table::as.data.table)
 aggregated$day <- aggregated$day[, day := format(date_bin_start, format = "%d %b %y")]
 aggregated$week <- aggregated$week[, week := format(date_bin_start, format = "%d %b %y")]
