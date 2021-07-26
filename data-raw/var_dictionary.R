91be9061ca4579fbff1f6df56fb1026db6bf7b45
Fernando Cagua <efcaguab@gmail.com>
2021-07-26 19:49:34 +1200

    Proper path of data dictionary

--- data-raw/var_dictionary.R
+++ data-raw/var_dictionary.R
@@ -1,4 +1,4 @@
 ## code to prepare `var_dictionary` dataset goes here
 
-var_dictionary <- yaml::read_yaml("inst/var_dictionary.yml")
+var_dictionary <- yaml::read_yaml("data-raw/var_dictionary.yml")
 usethis::use_data(var_dictionary, overwrite = TRUE)
