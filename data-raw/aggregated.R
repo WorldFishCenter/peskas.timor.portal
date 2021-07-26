6df24edbf78b18e420f3ff3fdf777fe78d678958
Fernando Cagua <efcaguab@gmail.com>
2021-07-26 19:33:58 +1200

    Add functions to download data from the public bucket

--- data-raw/aggregated.R
+++ data-raw/aggregated.R
@@ -1,5 +1,19 @@
 ## code to prepare `aggregated` dataset goes here
-aggregated <- readRDS("data-raw/aggregated.rds")
+pkgload::load_all()
+
+pars <- config::get(file = "inst/golem-config.yml")
+aggregated_rds <- cloud_object_name(
+  prefix = "timor_aggregated",
+  provider = pars$storage$google$key,
+  extension = "rds",
+  options = pars$storage$google$options,
+  exact_match = TRUE)
+
+download_cloud_file(name = aggregated_rds,
+                    provider = pars$storage$google$key,
+                    options = pars$storage$google$options)
+
+aggregated <- readRDS(aggregated_rds)
 aggregated <- lapply(aggregated, data.table::as.data.table)
 aggregated$day <- aggregated$day[, day := format(date_bin_start, format = "%d %b %y")]
 aggregated$week <- aggregated$week[, week := format(date_bin_start, format = "%d %b %y")]
