a8463f6668cf0bed2c5aebf362673e40d0191fc8
Fernando Cagua <efcaguab@gmail.com>
2021-08-21 12:39:20 +1200

    Add number of boats manually to the data

--- data-raw/aggregated.R
+++ data-raw/aggregated.R
@@ -16,6 +16,7 @@ aggregated <- readRDS(aggregated_rds)
 file.remove(aggregated_rds)
 
 aggregated <- lapply(aggregated, data.table::as.data.table)
+aggregated <- lapply(aggregated, function(x) x[, n_boats := 2334])
 aggregated$day <- aggregated$day[, day := format(date_bin_start, format = "%d %b %y")]
 aggregated$week <- aggregated$week[, week := format(date_bin_start, format = "%d %b %y")]
 aggregated$month <- aggregated$month[, month := format(date_bin_start, format = "%B %Y")][, year := format(date_bin_start, format = "%Y")]
