56a63e1a955ea7cb9e1ee1ee9e30ee6d60fc07ef
Fernando Cagua <efcaguab@gmail.com>
2021-08-22 17:10:11 +1200

    Add a function to plot series more simply

---
+++ R/plot_timeseries.R
@@ -0,0 +1,59 @@
+
+
+#' Plot series using apexcharter
+#'
+plot_timeseries <- function(x_categories, series, y_formatter, type = "bar", sparkline = F){
+  a <- apexcharter::apexchart() %>%
+    apexcharter::ax_chart(
+      type = type,
+      toolbar = list(show = FALSE),
+      sparkline = list(enabled = sparkline),
+      animations = list(enabled = FALSE),
+      stacked = FALSE,
+      selection = list(enabled = FALSE),
+      zoom = list(enabled = FALSE)) %>%
+    apexcharter::ax_dataLabels(
+      enabled = F
+    ) %>%
+    apexcharter::ax_series(series) %>%
+    apexcharter::ax_xaxis(
+      type = "datetime",
+      categories = x_categories,
+      labels =  list(rotate = 0,
+                     datetimeUTC = FALSE,
+                     padding = 0),
+      axisBorder = list(
+        show = FALSE
+      )) %>%
+    apexcharter::ax_yaxis(
+      labels = list(padding = 4,
+                    formatter = y_formatter)) %>%
+    apexcharter::ax_tooltip(
+      x = list(format = "MMM yyyy"),
+      y = list(formatter = y_formatter)) %>%
+    apexcharter::ax_plotOptions(
+      bar = list(columnWidth = "50%")) %>%
+    apexcharter::ax_responsive(
+      list(
+        breakpoint = 576,
+        options = list(
+          yaxis = list(show = FALSE)))
+    ) %>%
+    apexcharter::ax_colors("#206bc4", "#aaaaaa")
+
+  if (type != "bar") {
+    a <- a %>%
+      apexcharter::ax_stroke(curve = "smooth",
+                             width = "1.5")
+  }
+
+  if (isFALSE(sparkline)) {
+    a <- a %>%
+      apexcharter::ax_grid(
+        strokeDashArray = 4,
+        padding = list(
+          top = -20, right = 0, left = -4, bottom = -4))
+  }
+
+  a
+}
