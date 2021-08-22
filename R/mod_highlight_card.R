0b0561dcf65a4da2bd24bbc1629cade5c049358f
Fernando Cagua <efcaguab@gmail.com>
2021-08-22 17:12:30 +1200

    Highlight card to use plot_series and get_series_info

--- R/mod_highlight_card.R
+++ R/mod_highlight_card.R
@@ -33,67 +33,27 @@ mod_highlight_card_server <- function(id, var,
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
-    card_data <- reactiveVal(get_summary_info(var, period, n))
-
-    output$h <- renderText({
-      d <- card_data()
-      d$series_name
-    })
+    card_data <- reactiveVal(get_series_info(var, period, n))
 
     output$c <- apexcharter::renderApexchart({
 
       d <- card_data()
+      # We use the format of the first series overall
+      y_formatter = apexcharter::format_num(d$series[[1]]$series_format)
 
-      a <- apexcharter::apexchart() %>%
-        apexcharter::ax_chart(
-          type = type,
-          toolbar = list(show = FALSE),
-          sparkline = list(enabled = sparkline.enabled),
-          animations = list(enabled = FALSE),
-          stacked = FALSE,
-          selection = list(enabled = FALSE),
-          zoom = list(enabled = FALSE)) %>%
-        apexcharter::ax_dataLabels(
-          enabled = F
-        ) %>%
-        apexcharter::ax_series(
-          list(name = d$series_name,
-               data = d$series_y)) %>%
-        apexcharter::ax_xaxis(
-          type = "datetime",
-          categories = d$x_datetime,
-          labels =  list(rotate = 0,
-                         datetimeUTC = FALSE,
-                         padding = 0),
-          axisBorder = list(
-            show = FALSE
-          )) %>%
-        apexcharter::ax_yaxis(
-          labels = list(padding = 4,
-                        formatter = y_formatter)) %>%
-        apexcharter::ax_tooltip(
-          x = list(format = "MMM yyyy"),
-          y = list(formatter = y_formatter)) %>%
-        apexcharter::ax_grid(
-          strokeDashArray = 4,
-          padding = list(
-            top = -20, right = 0, left = -4, bottom = -4)) %>%
-        apexcharter::ax_plotOptions(
-          bar = list(columnWidth = "50%")) %>%
-        apexcharter::ax_responsive(
-          list(
-            breakpoint = 576,
-            options = list(
-              yaxis = list(show = FALSE)))
-        ) %>%
-        apexcharter::ax_colors("#206bc4", "#aaaaaa")
+      series <- lapply(d$series, function(x) {
+        list(
+          name = x$series_name,
+          data = x$series_value
+        )
+      })
 
-      if (type != "bar") {
-        a <- a %>%
-          apexcharter::ax_stroke(curve = "smooth",
-                                 width = "1.5")
-      }
-      a
+      plot_timeseries(
+        x_categories = d$x_datetime,
+        series = series[[1]],
+        y_formatter = y_formatter,
+        type = type,
+        sparkline = sparkline.enabled)
     })
 
   })
