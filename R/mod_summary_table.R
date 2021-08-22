60524dcf20485410ad1b4d073f816a12e8628609
Fernando Cagua <efcaguab@gmail.com>
2021-08-22 17:11:52 +1200

    Tweak summary table module so that it uses the new functions to get info

--- R/mod_summary_table.R
+++ R/mod_summary_table.R
@@ -7,12 +7,14 @@
 #' @noRd
 #'
 #' @importFrom shiny NS tagList
-mod_summary_table_ui <- function(id, ...){
-  ns <- NS(id)
-  tables <- get_yearly_tables("revenue")
-  years <- names(tables)
+mod_summary_table_ui <- function(id, years = NULL, ...){
 
-  s <- selectInput(ns("d"),
+  ns <- NS(id)
+  if (is.null(years)) {
+    years <- seq(as.numeric(format(Sys.Date(), "%Y")), "2018")
+  }
+
+  s <- selectInput(ns("y"),
                    label = "",
                    choices = years,
                    selectize = FALSE,
@@ -24,7 +26,8 @@ mod_summary_table_ui <- function(id, ...){
 
   tagList(
     table_card(
-      table = tableOutput(ns("t")),
+      # As an html output so we can tweak the alignment reactively
+      table = htmlOutput(ns("t")),
       dropdown = s,
       ...
     )
@@ -35,16 +38,33 @@ mod_summary_table_ui <- function(id, ...){
 #' summary_table Server Functions
 #'
 #' @noRd
-mod_summary_table_server <- function(id, var, format_fun = I){
+mod_summary_table_server <- function(id, vars, period = "month", format_fun = I){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
-    tables <- get_yearly_tables(var)
+    table <- reactive({
+      info <- get_series_info(vars, period, year = input$y)
+      data_columns <- lapply(info$series, function(x) {
+        vals <- data.table(d3.format::d3.format(x$series_format)(x$series_value))
+        names(vals) <- x$series_name
+        vals
+      })
+      if (period == "month"){
+        period_col <- data.table(period = format(as.Date(info$x_datetime), "%B"))
+      } else {
+        period_col <- data.table(period = info$x_categories)
+      }
 
-    output$t <- renderTable({
-      table <- tables[[input$d]]
-      table[]
-    }, spacing = "m", width = "100%")
+      cbind(period_col, Reduce(cbind, data_columns))
+    })
+
+    output$t <- renderUI({
+      table <- table()
+      alignment <- c("l", rep("r", ncol(table) - 1))
+      alignment <- paste(alignment, collapse = "")
+      output$table <- renderTable(table, spacing = "m", width = "100%", align = alignment)
+      tableOutput(ns('table'))
+      })
 
   })
 }
@@ -54,7 +74,7 @@ mod_summary_table_app <- function(){
     mod_summary_table_ui(id = "i")
   )
   server <- function(input, output, session) {
-    mod_summary_table_server("i")
+    mod_summary_table_server("i", c("revenue", "n_boats"))
   }
   shinyApp(ui, server)
 }
