42e9770b638e97fe19884f6a1433fb02ba31c046
Fernando Cagua <efcaguab@gmail.com>
2021-08-24 19:14:41 +1200

    Add a warning icon

--- R/ui_icons.R
+++ R/ui_icons.R
@@ -146,3 +146,26 @@ icon_currency_dollar <- function(size = 24){
     tags$path(d = "M12 3v3m0 12v3")
   )
 }
+
+icon_alert_triangle <- function(size = 24, class = ""){
+  class <- paste("icon icon-tabler icon-tabler-alert-triangle", class)
+  tags$svg(
+    xmlns = "http://www.w3.org/2000/svg",
+    class = class,
+    width = size,
+    height = size,
+    viewbox = "0 0 24 24",
+    `stroke-width` = "1.5",
+    stroke = "currentColor",
+    fill = "none",
+    `stroke-linecap` = "round",
+    `stroke-linejoin` = "round",
+    tags$path(
+      stroke = "none",
+      d = "M0 0h24v24H0z",
+      fill = "none"
+    ),
+    tags$path(d = "M12 9v2m0 4v.01"),
+    tags$path(d = "M5 19h14a2 2 0 0 0 1.84 -2.75l-7.1 -12.25a2 2 0 0 0 -3.5 0l-7.1 12.25a2 2 0 0 0 1.75 2.75")
+  )
+}
