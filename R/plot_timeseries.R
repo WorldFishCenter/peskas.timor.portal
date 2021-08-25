73f9afb9315989ca0b337247612de2d619b9ab6e
Fernando Cagua <efcaguab@gmail.com>
2021-08-25 15:50:42 +1200

    Add animations to apexcharts

--- R/plot_timeseries.R
+++ R/plot_timeseries.R
@@ -8,37 +8,38 @@ plot_timeseries <- function(x_categories, series, y_formatter, type = "bar", spa
       type = type,
       toolbar = list(show = FALSE),
       sparkline = list(enabled = sparkline),
-      animations = list(enabled = TRUE),
+      animations = list(
+        enabled = TRUE,
+        speed = 800,
+        animateGradually = list(enabled = TRUE)),
       stacked = FALSE,
       selection = list(enabled = FALSE),
       zoom = list(enabled = FALSE)) %>%
     apexcharter::ax_dataLabels(
-      enabled = F
-    ) %>%
+      enabled = F) %>%
     apexcharter::ax_series(series) %>%
     apexcharter::ax_xaxis(
       type = "datetime",
       categories = x_categories,
-      labels =  list(rotate = 0,
-                     datetimeUTC = FALSE,
-                     padding = 0),
+      labels =  list(
+        rotate = 0,
+        datetimeUTC = FALSE,
+        padding = 0),
       axisBorder = list(
-        show = FALSE
-      )) %>%
+        show = FALSE)) %>%
     apexcharter::ax_yaxis(
-      labels = list(padding = 4,
-                    formatter = y_formatter)) %>%
+      labels = list(
+        padding = 4,
+        formatter = y_formatter)) %>%
     apexcharter::ax_tooltip(
       x = list(format = "MMM yyyy"),
       y = list(formatter = y_formatter)) %>%
     apexcharter::ax_plotOptions(
       bar = list(columnWidth = "50%")) %>%
-    apexcharter::ax_responsive(
-      list(
-        breakpoint = 576,
-        options = list(
-          yaxis = list(show = FALSE)))
-    ) %>%
+    apexcharter::ax_responsive(list(
+      breakpoint = 576,
+      options = list(
+        yaxis = list(show = FALSE)))) %>%
     apexcharter::ax_colors("#206bc4", "#aaaaaa")
 
   if (type != "bar") {
