a08cc70e928a6354a489ab5edb047a4cb1ad50f7
Fernando Cagua <efcaguab@gmail.com>
2021-08-25 21:19:32 +1200

    Add bed and moon icons

--- R/ui_icons.R
+++ R/ui_icons.R
@@ -271,3 +271,52 @@ icon_arrow_down <- function(size = 24, class = ""){
   )
 }
 
+icon_moon <- function(size = 24, class = ""){
+  class <- paste("icon icon-tabler icon-tabler-moon", class)
+  tags$svg(
+    xmlns = "http://www.w3.org/2000/svg",
+    class = class,
+    width = size,
+    height = size,
+    viewbox = "0 0 24 24",
+    `stroke-width` = "1.5",
+    stroke = "currentColor",
+    fill = "none",
+    `stroke-linecap` = "round",
+    `stroke-linejoin` = "round",
+    tags$path(
+      stroke = "none",
+      d = "M0 0h24v24H0z",
+      fill = "none"
+    ),
+    tags$path(d = "M12 3c.132 0 .263 0 .393 0a7.5 7.5 0 0 0 7.92 12.446a9 9 0 1 1 -8.313 -12.454z")
+  )
+}
+
+
+icon_bed <- function(size = 24, class = ""){
+  class <- paste("icon icon-tabler icon-tabler-bed", class)
+  tags$svg(
+    xmlns = "http://www.w3.org/2000/svg",
+    class = class,
+    width = size,
+    height = size,
+    viewbox = "0 0 24 24",
+    `stroke-width` = "1.5",
+    stroke = "currentColor",
+    fill = "none",
+    `stroke-linecap` = "round",
+    `stroke-linejoin` = "round",
+    tags$path(
+      stroke = "none",
+      d = "M0 0h24v24H0z",
+      fill = "none"
+    ),
+    tags$path(d = "M3 7v11m0 -4h18m0 4v-8a2 2 0 0 0 -2 -2h-8v6"),
+    tags$circle(
+      cx = "7",
+      cy = "10",
+      r = "1"
+    )
+  )
+}
