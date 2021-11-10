2d9e27d897745e6fff5c5f0033a3b3d1a859ca77
Fernando Cagua <efcaguab@gmail.com>
2021-11-10 18:06:58 +1300

    Use markdown for the variable problems and methods

--- R/mod_var_descriptions.R
+++ R/mod_var_descriptions.R
@@ -47,15 +47,15 @@ mod_var_descriptions_server <- function(id, vars, i18n_r = reactive(list(t = fun
           content = tagList(
             markdown(
               i18n_r()$t(x[[1]]$description)),
-            tags$p(
+            tags$div(
               class = "small",
               tags$strong("Data processing and validation:"),
-              get_text(x[[1]]$methods, "None.")
+              get_text(x[[1]]$methods, tags$p("None."))
             ),
-            tags$p(
+            tags$div(
               class = "small",
               tags$strong("Known problems and limitations:"),
-              get_text(x[[1]]$problems, "Not assessed.")
+              get_text(x[[1]]$problems, tags$p("Not assessed."))
             ),
             tags$p(
               class = paste("badge mb-0", bg$light),
@@ -101,7 +101,7 @@ get_text <- function(x, y = "Not assessed."){
   if (is.null(x)) return(y)
   if (is.na(x)) return(y)
   if (x == "") return(y)
-  x
+  markdown(x)
 }
 
 mod_var_descriptions_app <- function(){
