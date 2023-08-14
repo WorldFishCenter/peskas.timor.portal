4087d5b09cbb5a0b9832b68d09dc43706460a4a1
langbart <lorenzo.longobardi@gmail.com>
2023-08-14 17:25:55 +0200

    Integrate download feature into about tab

--- R/timor_about.R
+++ R/timor_about.R
@@ -21,8 +21,8 @@ peskas_timor_about_ui <- function(id) {
             class = "card card-lg",
             tags$div(
               class = "card-body markdown",
-              htmlOutput(ns("about"))
-              # shiny::HTML('<script src="https://dataverse.harvard.edu/resources/js/widgets.js?alias=peskas&amp;dvUrl=https://dataverse.harvard.edu&amp;widgetScope=peskas&amp;widget=iframe&amp;heightPx=500"></script>')
+              htmlOutput(ns("about")),
+              shiny::HTML('<script src="https://dataverse.harvard.edu/resources/js/widgets.js?alias=peskas&amp;dvUrl=https://dataverse.harvard.edu&amp;widgetScope=peskas&amp;widget=iframe&amp;heightPx=500"></script>')
             )
           )
         )
